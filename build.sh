ddac61a90657fdb787bde9d62c33316fb2071746
wangcheng <wangcheng@jingyougroup.com>
2024-07-19 10:15:37 +0800

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 echo '开始拉取最新代码'
 git pull origin main
-echo '打包镜像'
-docker build -t zhulink:latest .
 echo '停止并删除旧容器'
 docker rm -f zhulink
+echo '删除镜像'
+docker images --filter=reference="zhulink" -a -q | xargs docker rmi -f
+echo '打包镜像'
+docker build -t zhulink:latest .
 echo '启动容器'
 docker run --name zhulink -d -v ./.env:/.env -p 32919:32919 --log-opt max-size=50m --restart=always zhulink:latest
