722a614e93a4fc414b83ae5472fe4d7bb254a7d7
王成 <wangcheng@jingyougroup.com>
2024-11-21 18:03:13 +0800

    修改等级算法

--- utils/util_level.go
+++ utils/util_level.go
@@ -8,16 +8,25 @@
 package utils
 
 import (
-	"math"
 	"strconv"
 )
 
-// GetUserLevel "根据用户积分判断用户等级，新注册用户默认等级0，每增加50等级增加1"
-// "<10时是等级零"
-// "10-50是等级一"
-// "50-100是等级二
-// "之后每增加50增加一个等级"
-func GetUserLevel(score int) string {
-	// 使用 math.Floor 向下取整
-	return strconv.Itoa(int(math.Floor(float64(score) / 50)))
+// GetUserLevel "根据用户积分判断用户等级，新注册用户默认等级0，每增加100等级增加1"
+// "<100时是等级零"
+// "100-200是等级一"
+// "每增加100增加一个等级"
+//
+//	func GetUserLevel(score int) string {
+//		// 使用 math.Floor 向下取整
+//		return strconv.Itoa(int(math.Floor(float64(score) / 100)))
+//	}
+func GetUserLevel(points int) string {
+	level := 0
+	threshold := 50 // 每级的积分门槛
+
+	for points >= threshold {
+		level++
+		threshold += 50 * level // 每升一级后，门槛递增
+	}
+	return strconv.Itoa(level)
 }
