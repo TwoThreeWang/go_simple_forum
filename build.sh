94b146430c28992243594404a4f12b1847e50173
WangTwoThree <twothreewang@gmail.com>
2025-02-28 11:42:33 +0800

    bulid

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ git pull origin main
 #docker images --filter=reference="zhulink" -a -q | xargs docker rmi -f
 echo '打包镜像'
 docker build -t zhulink:latest .
-echo '停止并删除旧容器'
-docker rm -f zhulink
 echo '启动容器'
-docker run --name zhulink -d -v ./.env:/.env -p 32919:32919 -v ./templates:/templates -v ./static:/static:rw --log-opt max-size=50m --restart=always zhulink:latest
+docker-compose up -d --remove-orphans
+#echo '停止并删除旧容器'
+#docker rm -f zhulink
+#echo '启动容器'
+#docker run --name zhulink -d -v ./.env:/.env -p 32919:32919 -v ./templates:/templates -v ./static:/static:rw --log-opt max-size=50m --restart=always zhulink:latest
 echo '清理不再使用的镜像、容器和数据卷'
 docker system prune --all --force --volumes
